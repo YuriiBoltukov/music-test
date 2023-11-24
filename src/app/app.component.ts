eda26b4a8fccb203e4af1f0afe4607cb174695d5
YuriiBoltukov <kolomiha898@gmail.com>
2023-11-24 18:35:03 +0400

    add material table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
 
+export interface Audio {
+  id: string;
+  name: string;
+  fileName: string;
+}
+
+const ELEMENT_DATA: Audio[] = [
+  {id: '1', name: 'Hydrogen', fileName: '1.0079'},
+  {id: '2', name: 'Helium', fileName: '4.0026', },
+  {id: '3', name: 'Lithium', fileName: '6.941',},
+  {id: '4', name: 'Beryllium', fileName: '9.0122', },
+  {id: '5', name: 'Boron', fileName: '10.811'},
+];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatButtonModule],
+  imports: [CommonModule, RouterOutlet, MatTableModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
 export class AppComponent {
-  title = 'music';
+  displayedColumns: string[] = ['demo-position', 'demo-name', 'demo-weight'];
+  dataSource = ELEMENT_DATA;
 }
